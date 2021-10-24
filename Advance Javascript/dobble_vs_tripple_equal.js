111108c783df1a6c6a9ed1076f834cad54a7c81a
codepundit1 <jahid35-266@diu.edu.bd>
2021-10-24 10:35:10 +0600

    dobble vs tripple equal is clrared for interview

---
+++ Advance Javascript/dobble_vs_tripple_equal.js
@@ -0,0 +1,12 @@
+// "==" don't cheack value type
+// "===" its checak value and type
+
+const num1 = 10;
+const num2 = "10";
+
+if(num1 === num2){
+    console.log("conditon is true")
+}
+else{
+    console.log("condition is false")
+}
