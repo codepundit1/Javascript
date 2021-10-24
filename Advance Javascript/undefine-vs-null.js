0ae9470fb0d1064599ab479c781b86efb732ec8b
codepundit1 <jahid35-266@diu.edu.bd>
2021-10-24 10:22:49 +0600

    null vs undefine is cleared for interview

---
+++ Advance Javascript/undefine-vs-null.js
@@ -0,0 +1,20 @@
+//Undefine
+
+//let number;
+// function add(num1, num2){
+//     console.log(num1+num2)
+//     return //something
+// }
+
+
+// function add(num1, num2){
+//     console.log(num1,num2)
+// }
+
+
+// const Doctor = {name: "Younus", Id : "1111"}
+// console.log(Doctor.gf);
+
+
+let fun = undefined;
+console.log(fun);
