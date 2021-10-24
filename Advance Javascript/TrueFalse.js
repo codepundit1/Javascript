bb8f589f937f7fede02021cc49b4b433953d54e3
codepundit1 <jahid35-266@diu.edu.bd>
2021-10-24 10:09:20 +0600

    after correction

--- Advance Javascript/TrueFalse.js
+++ Advance Javascript/TrueFalse.js
@@ -1,3 +1,17 @@
+// All are false value
+
+// false
+// 0
+// " "
+// undefine
+// NaN
+// null
+
+
+// Truthy
+//'0' , '' []
+
+
 const age = 5
 
 if(age>4){
@@ -7,3 +21,14 @@ if(age>4){
 else{
     console.log("False")
 }
+
+
+// const age = 0
+
+// if(age){
+//     console.log("True")
+// }
+
+// else{
+//     console.log("False")
+// }
